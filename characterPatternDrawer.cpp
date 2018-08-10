1e46759ad181af0e17258037ef6e26c930b5de65
Diego Benavides <diegob41@gmail.com>
2018-08-10 08:42:43 -0600

    Now main accepts a pattern parameter and selects according to user input parameters

--- characterPatternDrawer.cpp
+++ characterPatternDrawer.cpp
@@ -1,25 +1,35 @@
 #include <iostream>
 #include "PatternDrawer.h"
 #include "TreePatternFactory.h"
+#include "XPatternFactory.h"
 #include <string>
 
 int main(int argc, char **argv)
 {
 	PatternDrawer patternDrawer = PatternDrawer();
 	TreePatternFactory treePatternFactory = TreePatternFactory();
+	XPatternFactory xPatternFactory = XPatternFactory();
 
 	if (argc < 2) {
-		std::cerr << "Usage: " << argv[0] << " <row amount>" << std::endl;
+		std::cerr << "Usage: " << argv[0] << " tree|x <row amount>" << std::endl;
 		return -1;
 	}
 
-	int rowTotal = std::stoi(argv[1]);
+	std::string patternChoice = argv[1];
+	if (patternChoice == "tree") {
+		patternDrawer.setPatternFactory(&treePatternFactory);
+	} else if (patternChoice == "x") {
+		patternDrawer.setPatternFactory(&xPatternFactory);
+	} else {
+		std::cerr << "Invalid option " << patternChoice << std::endl;
+		return -1;
+	}
+
+	int rowTotal = std::stoi(argv[2]);
 	if (rowTotal < 0) {
 		std::cerr << "Row amount parameter needs to be greater or equal to zero" << std::endl;
 		return -1;
 	}
-
-	patternDrawer.setPatternFactory(&treePatternFactory);
 
 	patternDrawer.setRowTotal(rowTotal);
 
