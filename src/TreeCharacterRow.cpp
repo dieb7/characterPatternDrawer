d0fd8b0bf76ab0e9f09623a0311033cddcdeebf1
Diego Benavides <diegob41@gmail.com>
2018-08-10 00:09:27 -0600

    Now TreeCharacterRow is passing the tests

--- src/TreeCharacterRow.cpp
+++ src/TreeCharacterRow.cpp
@@ -8,6 +8,23 @@
 #include "TreeCharacterRow.h"
 
 std::string TreeCharacterRow::produceCharacters() {
+	if (!rowTotal) {
+		return std::string("");
+	}
 
-	return std::string();
+	std::string rowContent = "*";
+
+	// inner stars
+	for (int i = 0; i < rowIndex; i++) {
+		rowContent.append("*");
+		rowContent.append("*");
+	}
+
+	// outer padding
+	for (int i = 0; i < rowTotal - rowIndex - 1; i++) {
+		rowContent.insert(0, 1, ' ');
+		rowContent.append(" ");
+	}
+
+	return rowContent;
 }
